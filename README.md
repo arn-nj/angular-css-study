d15e626e60a32b868eac8f2d498326339fba7f66
arn-nj <iamaruun@gmail.com>
2021-06-09 18:02:10 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-f2zja1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-f2zja1)
---
+++ src/app/hero-app-main.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core';
+
+import { Hero } from './hero';
+
+@Component({
+  selector: 'app-hero-main',
+  template: `
+  <div class="theme-dark">
+    <app-quest-summary></app-quest-summary>
+    <app-hero-details [hero]="hero" [class.active]="hero.active">
+      <app-hero-controls [hero]="hero"></app-hero-controls>
+    </app-hero-details>
+    </div>
+  `
+})
+export class HeroAppMainComponent {
+  @Input() hero!: Hero;
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
